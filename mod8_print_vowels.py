8e5e4d304954aa9c953fccb4c6d1b72492e2032b
Francis Tsao <dialeleven@gmail.com>
2024-02-17 23:17:42 -0500

    printed out test string for context of vowels output

--- mod8_print_vowels.py
+++ mod8_print_vowels.py
@@ -1,6 +1,8 @@
 # print all vowels
 text = "The quick brown fox jumped over the lazy dog"
 
+print(f"Vowels in the text '{text}' are... ")
+
 for i in text:
    # check if letter is a vowel
    if "a" in i or "e" in i or "i" in i or "o" in i or "u" in i:
