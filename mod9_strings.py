7a12addd8fda92658bb915ee961044857874202d
Francis Tsao <dialeleven@gmail.com>
2024-02-19 23:07:34 -0500

    playing around with strings

--- mod9_strings.py
+++ mod9_strings.py
@@ -12,8 +12,33 @@ print(mylist[::-2])
 fruits = ['apple', 'cherry', 'banana', 'cherry']
 print ( fruits.count("cherry") )
 
-try:
-    print (fruits.index('appleZZZ'))
-except ValueError:
-    pass
-    #print('not in the list!')
+fruits = ['apple', 'orange', 'banana']
+fruits.insert(0, 'watermelon')
+print(fruits)
+
+fruits = ["apple", "banana", "cherry"]
+new_list = [1, 2, 3]
+fruits.append(new_list)
+print(fruits)
+
+fruits = ['apple', 'banana', 'cherry']
+cars = ['Ford', 'BMW', 'Volvo']
+fruits.extend(cars)
+print(fruits)
+
+mylist = ['apple', 12345, True, 3.14]
+print(type(mylist[1]))
+print(type(mylist[3]))
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+list3 = [7, 8, 9]
+combined_lists = [list1, list2, list3]
+
+for i in combined_lists:
+    for j in i:
+        print (j, end = ' ')
+    print()
+
+print(combined_lists[0][1])
+    #print(i)
