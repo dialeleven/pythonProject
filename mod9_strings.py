e88d30060197a13ef3b9536ffc949d7fd03344db
Francis Tsao <dialeleven@gmail.com>
2024-02-19 22:29:57 -0500

    bunch of changes. renamed the mod8 challenge file.

---
+++ mod9_strings.py
@@ -0,0 +1,16 @@
+mylist = [2, 4, 6, 8, 10]
+
+for i in mylist:
+    print(i)
+
+print(mylist[::])
+print(mylist[0:3:])
+print(mylist[2::])
+print(mylist[::-1])
+print(mylist[::-2])
+
+fruits = ['apple', 'cherry', 'banana', 'cherry']
+print ( fruits.count("cherry") )
+
+try:
+    print (fruits.index('appleZZZ'))
--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-# print all vowels
-text = "The quick brown fox jumped over the lazy dog"
+Name = 'Henry'
+print(Name[::-2])
 
-for i in text:
-   # check if letter is a vowel
-   if "a" in i or "e" in i or "i" in i or "o" in i or "u" in i:
-      print(i , end = '|')
