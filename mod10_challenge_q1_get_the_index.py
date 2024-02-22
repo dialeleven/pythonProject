1f5cf4e989726fa18eefd14dae63064253b23997
Francis Tsao <dialeleven@gmail.com>
2024-02-21 23:46:18 -0500

    renamed file

---
+++ mod10_challenge_q1_get_the_index.py
@@ -0,0 +1,5 @@
+def get_index(tup, elem):
+    ''' input:elem-indicates the element in the tuple,tup-indicates the tuple
+         output:Return the index of the element.'''
+    
+    # YOUR CODE GOES HERE
