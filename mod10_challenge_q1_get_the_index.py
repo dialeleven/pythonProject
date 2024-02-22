dffdb3853022173827b78fa7721588a7ab7f0f42
Francis Tsao <dialeleven@gmail.com>
2024-02-22 00:05:38 -0500

    Q1 complete

--- mod10_challenge_q1_get_the_index.py
+++ mod10_challenge_q1_get_the_index.py
@@ -3,3 +3,14 @@ def get_index(tup, elem):
          output:Return the index of the element.'''
     
     # YOUR CODE GOES HERE
+    # element value exists in tuple; check needed to avoid "ValueError: tuple.index(x): x not in tuple"
+    if elem in tup:
+        theindex = tup.index(elem)
+        return theindex
+    else:
+        return -1
+
+# two sample function calls below
+theindex = get_index((10, 20, 30, 40, 50), 50) # index exists (4)
+theindex = get_index((10, 20, 30, 40, 50), 100000) # index doesn't exist (-1)
+print(theindex)
