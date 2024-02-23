f98b36b46ee82a47f91a6101d69e3a8202e8a476
Francis Tsao <dialeleven@gmail.com>
2024-02-22 21:05:57 -0500

    Added problem description, overview, etc to bottom of each challenge for context

--- mod11_challenge_q1_sum_of_values.py
+++ mod11_challenge_q1_sum_of_values.py
@@ -9,3 +9,40 @@ def returnSum(dict):
     print(sum)
 
 returnSum({ 'x': 25, 'y': 25, 'z': 50})
+
+
+'''
+Sum of Values (https://www.scaler.com/topics/course/python-for-beginners/challenge/377544/challenge/?mode=read)
+
+Problem Description:
+
+Given a dictionary, find the sum of values of every key in the dictionary.
+
+
+Input Format:
+
+The input contains two lines.
+The first line has space-separated string values which are the keys of the dictionary.
+The second line has space-separated integer numbers which are the values of the dictionary.
+
+
+Output Format:
+
+Print the sum of the values of the items as an integer.
+
+
+Sample Input:
+
+x y z
+25 25 50
+
+
+Sample Output:
+
+100
+
+
+Output Explanation:
+
+The dictionary has three key-value pairs having values 25, 25, and 50. Hence their sum, 25+25+50 = 100 should be printed.
+'''
--- mod11_challenge_q2_adding_common_keys.py
+++ mod11_challenge_q2_adding_common_keys.py
@@ -28,3 +28,46 @@ dict2 = {'c': 4, 'd': 5, 'e': 6}
 #dict2 = {'nisarg': 1021516516, 'who': 4151651846} # expected return value ==> {'nisarg': 1021537867}
 
 commonKey(dict1, dict2)
+
+
+'''
+Adding Common Keys (https://www.scaler.com/topics/course/python-for-beginners/challenge/377544/challenge/?mode=read)
+
+Problem Description:
+
+Given two dictionaries, write a program for creating a dictionary in such a way that it consists of all the keys that are common in both dictionaries. The values corresponding to the keys in this new dictionary are the sum of the values of those keys in the two dictionaries.
+
+
+Input Format:
+
+The input contains four lines.
+The first line is space-separated string values which are the keys of the first dictionary.
+The second line is space-separated integer numbers which are values of the first dictionary.
+The third line is space-separated string values which are the keys of the second dictionary.
+The second line is space-separated integer numbers which are values of the second dictionary.
+
+
+Output Format:
+
+Print the resultant dictionary containing added values for common keys.
+
+
+Sample Input:
+
+a b c
+1 2 3
+
+c d e
+4 5 6
+
+
+Sample Output:
+
+{'c': 7}
+
+
+Output Explanation:
+
+Given the two dictionaries, {'a': 1, 'b': 2, 'c': 3} and {'c': 4, 'd':5. 'e', 6}, key 'c' is common having values 3 and 4. 
+Therefore, their sum 7 with the key 'c' should be added in to the resultant dictionary.
+'''
