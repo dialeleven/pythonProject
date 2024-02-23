9dfa8cefb1063b9b3a1f390b183a31129c17dcf3
Francis Tsao <dialeleven@gmail.com>
2024-02-22 22:53:42 -0500

    testing sets

--- mod11_sets.py
+++ mod11_sets.py
@@ -3,4 +3,16 @@ print(thisset)
 print(type(thisset))
 
 for item in thisset:
-    print(item)
+    print(item, end=' ')
+print()
+
+thisset.add('mango')
+
+for item in thisset:
+    print(item, end=' ')
+print()
+
+
+thisset = {"apple", "banana", "cherry"}
+thisset.remove('banana')
+print(thisset, end=' ')
