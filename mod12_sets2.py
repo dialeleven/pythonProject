3d722ab912c9da4766919c8efc893a02948b66fa
Francis Tsao <dialeleven@gmail.com>
2024-02-24 19:27:18 -0500

    Mod 12 challenge Q1 solution. sets test script additions.

--- mod12_sets2.py
+++ mod12_sets2.py
@@ -42,3 +42,51 @@ x = set(("apple", "banana", "cherry"))
 print(x)
 
 # Note: the set list is unordered, so the result will display the items in a random order.
+
+
+fruits = { 'apple' : 120, 'mango' : 100, 'banana' : 90 }
+new_fruits = { 'peach' : 130, 'kiwi' : 80 }
+fruits.update(new_fruits)
+print(fruits)
+
+sets = {3, 4, 5} 
+sets = sets.union({1, 2, 3})
+print(sets)
+
+
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple\n")
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+thisdict["color"] = "blue"
+print(thisdict)
+
+sets = {3, 4, 5} 
+sets.update([1, 2, 3])
+print(sets)
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+
+thisset.update(tropical)
+
+
+print(thisset)
+
+
+group1 = {'1', '2', '3'}
+group2 = {'4', '5', '6' }
+
+
+group3 = group1.union(group2)
+print(group3)
