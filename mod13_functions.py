4f8ec9a9fa82ebb40c54779f1e1351b337721c1b
Francis Tsao <dialeleven@gmail.com>
2024-02-25 20:38:45 -0500

    loops

--- mod13_functions.py
+++ mod13_functions.py
@@ -4,8 +4,57 @@ def greet(name):
     '''
     print(f'Hello {name}')
     '''
-    This is
+    This is a regular multi-line comment
     '''
 
 greet('Sally')
 print(greet.__doc__)
+
+
+def intro(name, age, hobby):
+    return name, age, hobby
+
+# return multiple values as individual variables
+a, b, c = intro('Sally', 30, 'Cycling')
+print(a, b, c)
+
+# return multiple values as a single return tuple "value"
+d = intro('Sally', 30, 'Cycling')
+print(d, '-', type(d))
+
+
+a = 5
+
+def func():
+    global a
+    a = 20
+    print(a)
+
+print(a) # a = 5 before func() call
+func() # a = 20 within func()
+print(a) # a= 20 outside func()
+
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+
+
+x = lambda a : a + 10
+print(x(5))
+
+y = lambda a, b : a + b
+print(y(3, 4))
