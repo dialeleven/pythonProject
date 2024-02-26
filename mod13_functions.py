853051d2643bb743fc4352a61afa6ab8f954bfd3
Francis Tsao <dialeleven@gmail.com>
2024-02-25 23:38:24 -0500

    testing

--- mod13_functions.py
+++ mod13_functions.py
@@ -74,3 +74,27 @@ print(larger(43, 5))
 z = lambda x, y : x if x > y else y
 print(z(50, 100))
 print(z(25, 10))
+
+
+# sorting a list with tuples
+lst = [(12, 56), (2, 4), (5,3)]
+
+# sorts based on the first index of each tuple (e.g. 2, 5, 12)
+lst.sort() 
+print(lst)
+#>>> [(2, 4), (5, 3), (12, 56)]
+
+# function to return index 1 of the passed value
+def getkey(x):
+   return x[1]
+
+# sort our list above [(12, 56), (2, 4), (5,3)] using the 2nd index (1)
+lst.sort(key = getkey)
+print(lst)
+#>>> [(5, 3), (2, 4), (12, 56)]
+
+
+# lambda version of above
+lst.sort(key = lambda x : x[1])
+print(lst)
+# >>> [(5, 3), (2, 4), (12, 56)]
