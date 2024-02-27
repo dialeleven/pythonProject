8fd77c9b41211deefe62bc00abde2fa18e2bba27
Francis Tsao <dialeleven@gmail.com>
2024-02-27 16:54:52 -0500

    Regular function vs. Lambda function to do the same task

--- mod13_functions_lamba_function2.py
+++ mod13_functions_lamba_function2.py
@@ -10,6 +10,7 @@ print(lst)
 def getkey(x):
    return x[1]
 
+
 # sort our list above [(12, 56), (2, 4), (5,3)] using the 2nd index (1)
 lst.sort(key = getkey)
 print(lst)
@@ -20,3 +21,21 @@ print(lst)
 lst.sort(key = lambda x : x[1])
 print(lst)
 # >>> [(5, 3), (2, 4), (12, 56)]
+
+'''
+The sort() method sorts the list ascending by default.
+
+You can also make a function to decide the sorting criteria(s).
+
+Syntax
+list.sort(reverse=True|False, key=myFunc)
+
+================
+Parameter Values
+================
+
+Parameter	    Description
+---------------------------
+reverse	        Optional. reverse=True will sort the list descending. Default is reverse=False
+key	            Optional. A function to specify the sorting criteria(s)
+'''
