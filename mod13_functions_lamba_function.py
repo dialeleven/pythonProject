f0a41ea4084b20a4e66754813429b26868f28fcf
Francis Tsao <dialeleven@gmail.com>
2024-02-27 16:59:20 -0500

    added syntax for lambda in script for reference

--- mod13_functions_lamba_function.py
+++ mod13_functions_lamba_function.py
@@ -2,3 +2,10 @@
 z = lambda x, y : x if x > y else y
 print(z(50, 100))
 print(z(25, 10))
+
+'''
+Syntax
+    lambda arguments : expression
+
+The expression is executed and the result is returned:
+'''
