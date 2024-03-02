6d34d6cae3103ae5f0f6b03ac4cd12df5e991015
Francis Tsao <dialeleven@gmail.com>
2024-03-02 18:59:15 -0500

    cool

--- mod15_file_handling5_binary_files.py
+++ mod15_file_handling5_binary_files.py
@@ -3,5 +3,6 @@ with open('mod15_test.png', 'rb') as f:
     data = f.read()
     print(data)
 
+    # take our existing binary data and write to a new copy of the file!
     with open('mod15_testnew.png', 'wb') as d:
         d.write(data)
